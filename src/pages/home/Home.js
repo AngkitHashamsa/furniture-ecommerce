80f2b39945193f72ce31c51b5941a492b2c59b59
Angkit <angkithashamsa21@gmail.com>
2022-04-23 01:49:45 +0530

    crousel added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Carousels from './carousels/Carousels';
+import Carousels from './carousels';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className="h-96">
       <Carousels />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatibus quod, odio, impedit dolorum tempora
+        et molestiae explicabo, eveniet similique perferendis delectus excepturi debitis reprehenderit recusandae quis
+        doloribus. Adipisci earum voluptatibus perspiciatis, minima, repellendus distinctio harum rerum hic doloribus
+        dolorem laudantium. Eum aperiam laborum voluptates autem repellendus. Explicabo, animi molestias.
+      </div>
     </div>
   );
 }
