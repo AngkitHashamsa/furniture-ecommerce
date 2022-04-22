37477978ae207f2f24f9b3ac1fe3148321e04fff
Angkit <angkithashamsa21@gmail.com>
2022-04-22 19:38:59 +0530

    tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // eslint-disable-next-line global-require
+  plugins: [require('tw-elements/dist/plugin')],
 };
